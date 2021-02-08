c252d50dc3b891df7d02d9701d89c60262e52787
smart-a <olalekan.adesoye@gmail.com>
2021-02-08 11:12:37 +0100

    First commit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.get("/" (req, res) => {
-    res.status(200).json({message: "Hello world!"})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello world!" });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listent on port ${port} ...`));
