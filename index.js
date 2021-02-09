93410ae3e93dbd2f35e73e1c4588853b011feeff
smart-a <olalekan.adesoye@gmail.com>
2021-02-09 10:23:59 +0100

    Adding mongoose to work with mongodb

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("./services/passport");
+const keys = require("./config/keys");
 const app = express();
+
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
 
 require("./routes/authRoutes")(app);
 
