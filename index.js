69b209667133b3f73a9bfd9336b6d9c2a75baa59
smart-a <olalekan.adesoye@gmail.com>
2021-02-15 12:47:29 +0100

    Preparing app for production to be deployed on Heroku

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
+const { ppid } = require("process");
 require("./models/User");
 require("./services/passport");
 
@@ -28,6 +29,16 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 require("./routes/billingRoute")(app);
+
+//Making the client side (React App) to work with Express App (Node App) only on production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listent on port ${port} ...`));
